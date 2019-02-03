91deeea245f215299b10cd2b5ec2ad0a86eaaf84
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2019-02-03 13:59:03 -0800

    first commit again

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoItems from "./TodoItems";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      list : [],
+      newItem : {}
+    }
+  }
+
+  buttonClick = () => {
+    console.log("clicked")
+    if(this.state.newItem.value!== "") {
+      let newArray = this.state.list.slice();
+      console.log("clicked2")
+      newArray.push(this.state.newItem);
+      console.log(this.state.newItem);
+      this.setState({list : newArray})
+    }
+  }
+
+
+  addToList = (event) => {
+    let newestItem = {
+      value: event.target.value,
+      key: Date.now()
+    }
+    this.setState({newItem: newestItem}) 
+  }
+
+
+  deleteItem = (key) => {
+    let filteredItems = this.state.list.filter(function (item) {
+      return (item.key!==key)
+    });
+    console.log(filteredItems)
+    this.setState({
+      list: filteredItems
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <header className="App-header">
+      <input onChange={(e) => this.addToList(e)} placeholder = "Enter item here"></input>
+      <button onClick={this.buttonClick}>Add Item</button>
+      <TodoItems entries={this.state.list}
+      delete={this.deleteItem}/>
+      </header>
       </div>
     );
   }
